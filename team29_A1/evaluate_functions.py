90d162be794d371b2794b7e1b19b85fcc2712887
Harmsen <t.p.harmsen@student.tue.nl>
2023-12-05 18:31:01 +0100

    improved overall functionality, currently working on implementing numpy for efficiency

--- team29_A1/evaluate_functions.py
+++ team29_A1/evaluate_functions.py
@@ -1,5 +1,6 @@
 from competitive_sudoku.sudoku import GameState, Move, SudokuBoard, TabooMove
 import competitive_sudoku.sudokuai
+import numpy as np
 
 def update_score(game_state, move, isMaximisingPlayer):
     """ Evaluate the given game state and return a reward.
@@ -10,10 +11,8 @@ def update_score(game_state, move, isMaximisingPlayer):
     N = game_state.board.N
     m = game_state.board.region_height()
     n = game_state.board.region_width()
-    if isMaximisingPlayer:
-        score = game_state.score[1]
-    else:
-        score = game_state.score[0]
+    
+    
     
     rewards = {0:0, 1:1, 2:3, 3:7}
     x = move.i
@@ -23,6 +22,12 @@ def update_score(game_state, move, isMaximisingPlayer):
     for i in range(N):
         if game_state.board.get(x, i) != SudokuBoard.empty:
             filled += 1
+
+    
+    print(check_row(game_state, x))
+    print(check_col(game_state, y))
+    print(check_box(game_state, x, y))
+    
     if filled == N-1: # if the amount of filled spots is N-1, the row is completed (after filling in the last spot)
         completed += 1
     filled = 0
@@ -38,12 +43,46 @@ def update_score(game_state, move, isMaximisingPlayer):
         if game_state.board.get(i, j) != SudokuBoard.empty:
             filled += 1
     if filled == N-1:
-        completed += 1                   
+        completed += 1
 
-    return score + rewards[completed]
-
-def evaluate(game_state, isMaximisingPlayer):
     if isMaximisingPlayer:
-        return game_state.score[0] - game_state.score[1]
+        game_state.scores[0] = game_state.scores[0] + rewards[completed]
     else:
-        return game_state.score[1] - game_state.score[0]
+        game_state.scores[1] = game_state.scores[1] + rewards[completed]
+
+
+def evaluate(game_state):
+        return game_state.scores[0] - game_state.scores[1]
+    
+def check_row(game_state, row: int):
+        """
+        Checks if the given row is completed.
+        @param row: A row value in the range [0, ..., N)
+        @return: True if the row is completed, False otherwise.
+        """
+        N = game_state.board.N
+        return game_state.board.squares[N*row:N*(row+1)].count(SudokuBoard.empty) == 1
+
+def check_col(game_state, col: int):
+        """
+        Checks if the given column is completed.
+        @param col: A column value in the range [0, ..., N)
+        @return: True if the row is completed, False otherwise.
+        """
+        N = game_state.board.N
+        column_indexes = np.arange(col, N**2, N) # get the indexes of the column
+        return np.count_nonzero(np.array(game_state.board.squares)[column_indexes]) == N - 1
+
+def check_box(game_state, row: int, col: int):
+        """
+        Checks if the given region box in the sudoku is completed.
+        @param row: A row value in the range [0, ..., N)
+        @param col: A column value in the range [0, ..., N)
+        @return: True if the row is completed, False otherwise.
+        """
+        N = game_state.board.N
+        m = game_state.board.region_height()
+        n = game_state.board.region_width()
+        print(N, m, n)
+        region = [[((row + inc_m) % m) + (row // m)*m, ((col + inc_n) % n) + (col // n)*n] for inc_m in range(1,m+1) for inc_n in range(1,n+1)] # for finding the coordinates within the region
+        return np.count_nonzero(np.array(game_state.board.squares)[region]) == N - 1
