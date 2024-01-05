eacff1c36a485224e42dcfa1b77c0a64e03c35eb
tpharmsen <t.p.harmsen@student.tue.nl>
2024-01-05 16:45:01 +0100

    MCMC setup

---
+++ team29_A3_MCMC/sudokuai.py
@@ -0,0 +1,196 @@
+#  (C) Copyright Wieger Wesselink 2021. Distributed under the GPL-3.0-or-later
+#  Software License, (See accompanying file LICENSE or copy at
+#  https://www.gnu.org/licenses/gpl-3.0.txt)
+
+import random
+import time
+from competitive_sudoku.sudoku import GameState, Move, SudokuBoard, TabooMove
+import competitive_sudoku.sudokuai
+import numpy as np
+from collections import defaultdict
+
+
+class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
+    """
+    Sudoku AI that computes a move for a given sudoku configuration.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    # N.B. This is a very naive implementation.
+    def compute_best_move(self, game_state: GameState) -> None:
+        N = game_state.board.N
+
+        def possible(i, j, value):
+            return game_state.board.get(i, j) == SudokuBoard.empty \
+                   and not TabooMove(i, j, value) in game_state.taboo_moves
+
+        all_moves = [Move(i, j, value) for i in range(N) for j in range(N)
+                     for value in range(1, N+1) if possible(i, j, value)]
+        move = random.choice(all_moves)
+        self.propose_move(move)
+        while True:
+            time.sleep(0.2)
+            self.propose_move(random.choice(all_moves))
+
+def main():
+    root = MonteCarloTreeSearchNode(state = initial_state)
+    selected_node = root.best_action()
+    return 
+
+class MonteCarloTreeSearchNode():
+    def __init__(self, state, parent=None, parent_action=None):
+        self.state = state
+        self.parent = parent
+        self.parent_action = parent_action
+        self.children = []
+        self._number_of_visits = 0
+        self._results = defaultdict(int)
+        self._results[1] = 0
+        self._results[-1] = 0
+        self._untried_actions = None
+        self._untried_actions = self.untried_actions()
+        return
+    
+    def untried_actions(self):
+
+        self._untried_actions = self.state.get_legal_actions()
+        return self._untried_actions
+
+    def q(self):
+        wins = self._results[1]
+        loses = self._results[-1]
+        return wins - loses
+
+    def n(self):
+        return self._number_of_visits
+
+    def expand(self):
+        
+        action = self._untried_actions.pop()
+        next_state = self.state.move(action)
+        child_node = MonteCarloTreeSearchNode(
+            next_state, parent=self, parent_action=action)
+
+        self.children.append(child_node)
+        return child_node 
+
+    def is_terminal_node(self):
+        return self.state.is_game_over()
+
+
+    def rollout(self):
+        current_rollout_state = self.state
+        
+        while not current_rollout_state.is_game_over():
+            
+            possible_moves = current_rollout_state.get_legal_actions()
+            
+            action = self.rollout_policy(possible_moves)
+            current_rollout_state = current_rollout_state.move(action)
+        return current_rollout_state.game_result()
+
+    def backpropagate(self, result):
+        self._number_of_visits += 1.
+        self._results[result] += 1.
+        if self.parent:
+            self.parent.backpropagate(result)
+
+
+    def is_fully_expanded(self):
+        return len(self._untried_actions) == 0
+
+    def best_child(self, c_param=0.1):
+        
+        choices_weights = [(c.q() / c.n()) + c_param * np.sqrt((2 * np.log(self.n()) / c.n())) for c in self.children]
+        return self.children[np.argmax(choices_weights)]
+
+
+    def rollout_policy(self, possible_moves):
+        
+        return possible_moves[np.random.randint(len(possible_moves))]
+
+    def _tree_policy(self):
+
+        current_node = self
+        while not current_node.is_terminal_node():
+            
+            if not current_node.is_fully_expanded():
+                return current_node.expand()
+            else:
+                current_node = current_node.best_child()
+        return current_node
+
+    def best_action(self):
+        simulation_no = 100
+        
+        
+        for i in range(simulation_no):
+            
+            v = self._tree_policy()
+            reward = v.rollout()
+            v.backpropagate(reward)
+        
+        return self.best_child(c_param=0.)
+
+
+    def get_legal_actions(self): 
+        '''
+        Modify according to your game or
+        needs. Constructs a list of all
+        possible actions from current state.
+        Returns a list.
+        '''
+
+    def is_game_over(self, board):
+        '''
+        Modify according to your game or 
+        needs. It is the game over condition
+        and depends on your game. Returns
+        true or false
+        ''' 
+        return not np.isin(0, board.squares)
+
+    def game_result(self, game_state):
+        '''
+        Modify according to your game or 
+        needs. Returns 1 or 0 or -1 depending
+        on your state corresponding to win,
+        tie or a loss.
+        '''
+        our_agent = game_state.our_agent
+        score_diff = game_state.scores[our_agent] - game_state.scores[not our_agent]
+        if score_diff > 0:
+            return 1
+        elif score_diff == 0:
+            return 0
+        else:    
+            return -1
+        
+
+    def move(self,action):
+        '''
+        Modify according to your game or 
+        needs. Changes the state of your 
+        board with a new value. For a normal
+        Tic Tac Toe game, it can be a 3 by 3
+        array with all the elements of array
+        being 0 initially. 0 means the board 
+        position is empty. If you place x in
+        row 2 column 3, then it would be some 
+        thing like board[2][3] = 1, where 1
+        represents that x is placed. Returns 
+        the new state after making a move.
+        '''
+
+        for move in self.get_valid_moves(game_state):
+            game_state_copy = copy.deepcopy(game_state) # deepcopy gamestate
+
+            game_state_copy.moves.append(move) # add move to moves list
+            update_scores(game_state_copy, move,
+                            is_maximizing_player)  # update scores
+            game_state_copy.board.put(
+                move.i, move.j, move.value)  # fill in the move
+            children_states.append(game_state_copy) # add child state to list of child states
+        return children_states
